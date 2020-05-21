a9a40be64b20146577ab2589ecd7e18dff9608b3
Anyan Xie <anyanxie@gmail.com>
2020-05-21 14:50:40 -0400

    object and inheritance

--- functional.js
+++ functional.js
@@ -3,7 +3,7 @@
 // on a hyphen, like this:
 // "North Dakota" -> "north-dakota"
 
-// my version
+///////////////// my version
 
 function toHyphenString(string) {
 	let lowerCased = new Array();
@@ -17,7 +17,7 @@ function toHyphenString(string) {
 let input = 'North Dakota';
 console.log(toHyphenString(input));
 
-// sample answer
+///////////////// textbook version
 let states = [ 'Kansas', 'Nebraska', 'North Dakota', 'South Dakota' ];
 
 // Returns a URL-friendly version of a string.
@@ -41,3 +41,50 @@ function functionalUrls(elements) {
 	return elements.map((element) => urlify(element));
 }
 console.log(functionalUrls(states));
+
+// singles: Functional version
+function functionalSingles(elements) {
+	return elements.filter((element) => element.split(/\s+/).length === 1);
+}
+console.log(functionalSingles(states));
+
+///////////// reduce function
+
+let numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+// sum: Imperative solution
+function imperativeSum(elements) {
+	let total = 0;
+	elements.forEach(function(n) {
+		total += n;
+	});
+	return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum: Functional solution
+function functionalSum(elements) {
+	return elements.reduce((total, n) => {
+		return (total += n);
+	});
+}
+console.log(functionalSum(numbers));
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+	let lengths = {};
+	elements.forEach(function(element) {
+		lengths[element] = element.length;
+	});
+	return lengths;
+}
+console.log(imperativeLengths(states));
+
+// lengths: Functional solution
+function functionalLengths(elements) {
+	return elements.reduce((lengths, element) => {
+		lengths[element] = element.length;
+		return lengths;
+	}, {});
+}
+console.log(functionalLengths(states));
