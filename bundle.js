48fed9025ab8264ad3f5aa61852b1c843dc84560
Anyan Xie <anyanxie@gmail.com>
2020-05-21 22:22:06 -0400

    update

--- bundle.js
+++ bundle.js
@@ -3,14 +3,25 @@ let Phrase = require('mhartl-palindrome');
 
 //alert(new Phrase("Madam, I'm Adam.").palindrome());
 
-let string = prompt('Please enter a string for palindrome testing:');
-let phrase = new Phrase(string);
+function palindromeTester(event) {
+	// prevent default form submission
+	event.preventDefault();
+	let phrase = new Phrase(event.target.phrase.value);
+	let palindromeResult = document.querySelector('#palindromeResult');
 
-if (phrase.palindrome()) {
-	alert(`"${phrase.content}" is a palindrome!`);
-} else {
-	alert(`"${phrase.content}" is not a palindrome.`);
+	if (phrase.palindrome()) {
+		palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+	} else {
+		palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+	}
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+	let tester = document.querySelector('#palindromeTester');
+	tester.addEventListener('submit', function(event) {
+		palindromeTester(event);
+	});
+});
 
 },{"mhartl-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
